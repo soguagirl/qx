e5dce40f585d74b676ab8f0942b1a8b94296015a
gongzhen.gz <gongzhen.gz@alibaba-inc.com>
2018-07-04 22:06:06 +0800

    about

--- app/js/index.js
+++ app/js/index.js
@@ -1,6 +1,22 @@
-var myFullpage = new fullpage('#app', {
+
+var app = null;
+var logo = null;
+var section3Title = null;
+var section3SubTitle = null;
+var section3Partners = null;
+
+window.onload = function () {
+  app = document.getElementById('app');
+  logo = document.getElementById('logoImg');
+  var cooperation = document.getElementById('cooperation');
+  section3Title = cooperation.getElementsByClassName('title')[0];
+  section3SubTitle = cooperation.getElementsByClassName('sub-title')[0];
+  section3Partners = cooperation.getElementsByClassName('partners')[0];
+}
+
+var myFullpage = new fullpage('#fullpage', {
   // sectionsColor: ['#1bbc9b', '#4BBFC3', '#7BAABE', 'whitesmoke'],
-  anchors: ['page1', 'page2', 'page3', 'page4'],
+  anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
   menu: '#menu',
   continuousVertical: false,
   navigation: true,
@@ -12,15 +28,22 @@ var myFullpage = new fullpage('#app', {
         direction: direction
     };
 
-    var app = document.getElementById('app');
-    var logo = document.getElementById('logoImg');
+    if (app && logo) {
+      if (destination.index >= 2) {
+        logo.src = '../assets/images/logo2.png';
+        app.className = 'light';
+      } else {
+        logo.src = '../assets/images/logo1.png';
+        app.className = 'dark';
+      }
+    }
 
-    if (destination.index >= 2) {
-      logo.src = '../assets/images/logo2.png';
-      app.className = 'light';
-    } else {
-      logo.src = '../assets/images/logo1.png';
-      app.className = 'dark';
+    if (destination.index === 2) {
+      if (section3Title) {
+        section3Title.style.webkitAnimationPlayState = "running";
+        section3SubTitle.style.webkitAnimationPlayState = "running";
+        section3Partners.style.webkitAnimationPlayState = "running";
+      }
     }
 
     console.log("--- afterLoad ---");
